c82af9922bef18e53e4abe658ebbdff8bb507cb8
Stefan Ivanov <ivanov@Stefans-MacBook-Air.local>
2023-06-19 00:55:47 +0300

    -

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+const PORT = process.env.PORT || 5001;
 
 app.use(express.static('public'));
 
@@ -13,4 +14,4 @@ app.get('/test', function(req, res) {
   res.sendFile(path.join(__dirname, '/pages/test.html'));
 });
 
-app.listen(3000)
+app.listen(PORT, () => console.log(`Listen on port ${PORT}`));
