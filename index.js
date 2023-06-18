93df47d41391c596d5bc2e31936f1a1672d924dc
Stefan Ivanov <ivanov@Stefans-MacBook-Air.local>
2023-06-19 01:17:27 +0300

    -

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // sendFile will go here
 app.get('/', function(req, res) {
